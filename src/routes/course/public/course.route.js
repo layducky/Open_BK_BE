7dfddc8e7fa94057f25f50e279be3b6ac4105f8a
layducky <tranphuocnhatpnt@gmail.com>
2026-02-24 20:59:05 +0700

    fix: guest able to view course content

--- src/routes/course/public/course.route.js
+++ src/routes/course/public/course.route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const CourseController = require('../../../controllers/course/course.controller');
+const UnitController = require('../../../controllers/course/unit.controller');
 const { verifyJWT } = require('../../../middleware/verifyJWT');
 
 const router = express.Router();
 
 router.get('/', CourseController.getAllCourses);
 router.get('/categories', CourseController.getCategories);
+router.get('/:courseID/units', UnitController.getAllUnits);
 router.get('/:courseID', CourseController.getCourseByID);
 
 router.use(verifyJWT(["ADMIN"]));
